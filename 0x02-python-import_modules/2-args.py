a6046271941de96d99d4c7ce68bc58185f52ea73
Matthew Brinkmann <matthew.brinkmann@hotmail.com>
2022-04-27 15:35:16 -0700

    fix: import running issues

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,22 +1,20 @@
 #!/usr/bin/python3
 import sys
 
-if __name__ != "__main__":
-    exit()
+if __name__ == "__main__":
+    argc = len(sys.argv) - 1
+    string1 = "{} argument".format(argc)
+    if argc == 0:
+        string1 += 's.'
+    elif argc == 1:
+        string1 += ':'
+    else:
+        string1 += 's:'
 
-argc = len(sys.argv) - 1
-string1 = "{} argument".format(argc)
-if argc == 0:
-    string1 += 's.'
-elif argc == 1:
-    string1 += ':'
-else:
-    string1 += 's:'
+    print(string1)
 
-print(string1)
-
-i = 0
-for arg in sys.argv:
-    if i != 0:
-        print(f"{i}: {arg}")
-    i += 1
+    i = 0
+    for arg in sys.argv:
+        if i != 0:
+            print(f"{i}: {arg}")
+        i += 1
