e738abe17d863c673a0efa7f0081a820e3f2444c
Matthew Brinkmann <matthew.brinkmann@hotmail.com>
2022-04-27 00:47:09 -0700

    fix: error caused if adding to a blank list or adding to start of list

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -3,6 +3,28 @@
 #include "lists.h"
 
 /**
+ * build_node - insert a node in a sorted list
+ *
+ * @too: when this node points too
+ * @number: the valuse used to list place and to add as value into list element
+ *
+ * Return: address to new element
+ */
+listint_t *build_node(int number, listint_t *too)
+{
+	listint_t *new = NULL;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = number;
+	new->next = too;
+
+	return (new);
+}
+
+/**
  * insert_node - insert a node in a sorted list
  *
  * @head: the pointer to the first element in the list
@@ -16,8 +38,21 @@ listint_t *insert_node(listint_t **head, int number)
 
 	if (head == NULL)
 		return (NULL);
+	if (*head == NULL)
+	{
+		new = build_node(number, NULL);
+		*head = new;
+		return (new);
+	}
+
 	search1 = *head;
 	search2 = *head;
+	if (search1->n >= number)
+	{
+		new = build_node(number, search1);
+		*head = new;
+		return (new);
+	}
 
 	while (search2->n < number && search2 != NULL)
 	{
@@ -29,15 +64,10 @@ listint_t *insert_node(listint_t **head, int number)
 	}
 
 	if (search2->next == NULL)
-		return(add_nodeint_end(head, number));
+		return (add_nodeint_end(head, number));
 
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
-		return (NULL);
-
-	new->n = number;
+	new = build_node(number, search2);
 	search1->next = new;
-	new->next = search2;
 
 	return (new);
 }
