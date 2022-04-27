6cdffce62d0981d22c764645100e1b3e65255b3b
Matthew Brinkmann <matthew.brinkmann@hotmail.com>
2022-04-26 20:03:01 -0700

    fixing find absolute value for negative number

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-lastDig = int(repr(number)[-1])
+lastDig = abs(int(repr(number)[-1]))
 if lastDig == 0:
     print(f"Last digit of {number} is {lastDig} and is 0")
 elif lastDig != 0 and lastDig < 6:
