9efaf300299d35a9bff191730ea96f31362a6042
Matthew Brinkmann <matthew.brinkmann@hotmail.com>
2022-04-26 20:11:04 -0700

    fixing task 1 style error

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-lastDig = abs(int(repr(number)[-1]))
-if lastDig == 0:
-    print(f"Last digit of {number} is {lastDig} and is 0")
-elif lastDig != 0 and lastDig < 6:
-    print(f"Last digit of {number} is {lastDig} and is less than 6 and not 0")
+x = int(repr(number)[-1])
+if x == 0:
+    print(f"Last digit of {number} is {x} and is 0")
+elif x != 0 and x < 6 or number < 0:
+    if number < 0:
+        print(f"Last digit of {number} is {-x} and is less than 6 and not 0")
+    else:
+        print(f"Last digit of {number} is {x} and is less than 6 and not 0")
 else:
-    print(f"Last digit of {number} is {lastDig} and is greater than 5")
+    print(f"Last digit of {number} is {x} and is greater than 5")
