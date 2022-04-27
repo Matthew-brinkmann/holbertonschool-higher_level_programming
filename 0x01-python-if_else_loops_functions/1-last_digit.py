89c3c999ef4fc2851973e45d3451e576fd42c5b2
Matthew Brinkmann <matthew.brinkmann@hotmail.com>
2022-04-26 20:21:47 -0700

    fixing task 1 to remove all outside functions

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,13 +1,21 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-x = int(repr(number)[-1])
+
+if number < 0:
+    hold = -(number)
+else:
+    hold = number
+
+x = hold % 10
+
 if x == 0:
-    print(f"Last digit of {number} is {x} and is 0")
+    string = " and is 0"
 elif x != 0 and x < 6 or number < 0:
     if number < 0:
-        print(f"Last digit of {number} is {-x} and is less than 6 and not 0")
-    else:
-        print(f"Last digit of {number} is {x} and is less than 6 and not 0")
+        x = -(x)
+    string = " and is less than 6 and not 0"
 else:
-    print(f"Last digit of {number} is {x} and is greater than 5")
+    string = " and is greater than 5"
+
+print(f"Last digit of {number} is {x}" + string)
