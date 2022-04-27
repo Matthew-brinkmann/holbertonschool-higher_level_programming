b200e39f60bf3e971faf9c27f804a956d4bd90d3
Matthew Brinkmann <matthew.brinkmann@hotmail.com>
2022-04-27 15:33:07 -0700

    adding task 3

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ != "__main__":
+    exit()
+
+argc = len(sys.argv) - 1
+string1 = "{} argument".format(argc)
+if argc == 0:
+    string1 += 's.'
+elif argc == 1:
+    string1 += ':'
+else:
+    string1 += 's:'
+
+print(string1)
+
+i = 0
+for arg in sys.argv:
+    if i != 0:
+        print(f"{i}: {arg}")
+    i += 1
