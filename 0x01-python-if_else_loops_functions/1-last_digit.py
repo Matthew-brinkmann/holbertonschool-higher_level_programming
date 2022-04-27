33ae864efc040bd79dacc45a8659d2ef4da5c487
Matthew Brinkmann <matthew.brinkmann@hotmail.com>
2022-04-26 19:42:14 -0700

    ading task 1, pycodestyle compliant

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-lastDigit = int(repr(number)[-1])
-if lastDigit == 0:
-    print(f"Last digit of {number} is {lastDigit} and is 0")
-elif lastDigit != 0 and lastDigit < 6 :
-    print(f"Last digit of {number} is {lastDigit} and is less than 6 and not 0")
+lastDig = int(repr(number)[-1])
+if lastDig == 0:
+    print(f"Last digit of {number} is {lastDig} and is 0")
+elif lastDig != 0 and lastDig < 6:
+    print(f"Last digit of {number} is {lastDig} and is less than 6 and not 0")
 else:
-    print(f"Last digit of {number} is {lastDigit} and is greater than 5")
+    print(f"Last digit of {number} is {lastDig} and is greater than 5")
