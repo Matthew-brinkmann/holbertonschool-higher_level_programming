66584096ef5ed3d631653fbf8279bc2fb4d232cf
Matthew Brinkmann <matthew.brinkmann@hotmail.com>
2022-04-27 19:15:29 -0700

    changing the format used on the print statement

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 from calculator_1 import add, sub, mul, div
+
 if __name__ == "__main__":
     a = 10
     b = 5
-    print(f"{a} + {b} = {add(a, b)}")
-    print(f"{a} - {b} = {sub(a, b)}")
-    print(f"{a} * {b} = {mul(a, b)}")
-    print(f"{a} / {b} = {div(a, b)}")
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
