154f8a75a9f89df734220ec765cb17c8b617837b
Matthew Brinkmann <matthew.brinkmann@hotmail.com>
2022-05-01 16:39:31 -0700

    build: adding task 7

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,21 @@
+#!/bin/usr/python3
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    new_tup = ()
+    for i in range(2):
+        if i >= len(tuple_a):
+            val1 = 0
+        else:
+            val1 = tuple_a[i]
+
+        if i >= len(tuple_b):
+            val2 = 0
+        else:
+            val2 = tuple_b[i]
+
+        if i == 0:
+            new_tup = (val1 + val2)
+        else:
+            new_tup = (new_tup, (val1 + val2))
+
+    return (new_tup)
