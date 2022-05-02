908e9bebad8edbd551f968b10ea7fd6dcb17a51d
Matthew Brinkmann <matthew.brinkmann@hotmail.com>
2022-05-01 19:38:37 -0700

    fix: change f-string to stringg.format

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -5,4 +5,4 @@ def print_reversed_list_integer(my_list=[]):
         new_list = my_list[0:]
         new_list.reverse()
         for i in range(len(new_list)):
-            print(f"{new_list[i]}")
+            print("{:d}".format(new_list[i]))
