9f5e72beea5646697d71a55db20e1127674c7a05
Matthew Brinkmann <matthew.brinkmann@hotmail.com>
2022-05-01 19:30:04 -0700

    fix: add check to ensure that my_list is actually of type list before calling list.reverse

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
-    new_list = my_list[0:]
-    new_list.reverse()
-    for i in range(len(new_list)):
-        print(f"{new_list[i]}")
+    if type(my_list) is list:
+        new_list = my_list[0:]
+        new_list.reverse()
+        for i in range(len(new_list)):
+            print(f"{new_list[i]}")
