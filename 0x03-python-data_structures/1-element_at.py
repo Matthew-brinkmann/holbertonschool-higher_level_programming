7a9b4d7e2e73c57f67b7747f36487f196bfe1b65
Matthew Brinkmann <matthew.brinkmann@hotmail.com>
2022-05-01 19:22:57 -0700

    fix: included a variable, instead of calling function in if statement

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 def element_at(my_list, idx):
-    if idx < 0 or idx > len(my_list):
+    list_len = len(my_list)
+    if idx < 0 or idx > list_len:
         return
     return (my_list[idx])
