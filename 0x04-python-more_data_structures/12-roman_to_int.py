585a929516ca548c3bfd53819c0b7e5268fb1a63
Matthew Brinkmann <matthew.brinkmann@hotmail.com>
2022-05-03 19:27:34 -0700

    fix: removed unused variable

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     romConv = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
-    prevNum = 0
     retVal = 0
     intHold = []
     if roman_string is None or type(roman_string) is not str:
