3c25e3db7f591c1405d7dbbe38f361a05808cfc8
Matthew Brinkmann <matthew.brinkmann@hotmail.com>
2022-05-15 18:30:57 -0700

    convert: from format type string to f-type string

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -6,5 +6,5 @@ def safe_print_integer_err(value):
         print("{:d}".format(value))
         return (True)
     except (ValueError, TypeError) as e:
-        print("Exception: {}".format(e), file=sys.stderr)
+        print(f"Exception: {e}", file=sys.stderr)
         return (False)
