826ab65c928dbc3cee1d73bf12374f854035a4e8
Matthew Brinkmann <matthew.brinkmann@hotmail.com>
2022-05-15 18:31:49 -0700

    convert: from format type string to f-type string

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -6,5 +6,5 @@ def safe_function(fct, *args):
     try:
         return(fct(*args))
     except Exception as e:
-        print("Exception: {}".format(e), file=sys.stderr)
+        print(f"Exception: {e}", file=sys.stderr)
         return (None)
