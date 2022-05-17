abe2c3fa7acdb0eb6657890fa40c9ce855f8ebf9
Matthew Brinkmann <matthew.brinkmann@hotmail.com>
2022-05-17 18:28:09 +1000

    fix: add descriptions

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
+"""defines a class square"""
+
+
 class Square:
+    """
+    class for Square
+    """
     def __init__(self, size=0):
+          """
+        Init class with args: size
+        runs a check to ensure size data is correct type and value.
+        """
         if type(size) != int:
             raise TypeError("size must be an integer")
         if size < 0:
