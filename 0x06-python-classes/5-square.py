7b56da57371744372c0bde8677f36f502479c928
Matthew Brinkmann <matthew.brinkmann@hotmail.com>
2022-05-17 10:10:56 +1000

    fixing more broken fixes

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -7,11 +7,14 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = size
+
     def area(self):
         return (self.__size * self.__size)
+
     @property
     def size(self):
         return (self.__size)
+
     @size.setter
     def size(self, value):
         if type(value) != int:
@@ -20,6 +23,7 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = value
+
     def my_print(self):
         for i in range(self.__size):
             for x in range(self.__size):
