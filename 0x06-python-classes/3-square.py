f91ef90731ff12e57c2857008190df8ef06c0376
Matthew Brinkmann <matthew.brinkmann@hotmail.com>
2022-05-17 18:31:07 +1000

    fix: add descriptions

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
+"""defines a class square"""
+
+
 class Square:
+    """
+    class for Square
+    """
     def __init__(self, size=0):
+        """
+        Init class with args: size
+        runs a check to ensure size data is correct type and value.
+        """
         if type(size) != int:
             raise TypeError("size must be an integer")
         if size < 0:
@@ -9,4 +19,7 @@ class Square:
             self.__size = size
 
     def area(self):
+        """
+        returns the area size of the square
+        """
         return (self.__size * self.__size)
