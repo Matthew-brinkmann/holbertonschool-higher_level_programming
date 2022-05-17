a3eb4d999bff8f1213a8114b63382b70ea572ba1
Matthew Brinkmann <matthew.brinkmann@hotmail.com>
2022-05-17 18:26:42 +1000

    fix: add descriptions

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
+"""defines a class square"""
+
+
 class Square:
+    """
+    class for Square
+    """
     def __init__(self, size):
+        """
+        Init class with args: size
+        """
         self.__size = size
