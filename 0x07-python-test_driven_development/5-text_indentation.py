130bd41f963041007432b19dbe260545b664aa56
Matthew Brinkmann <matthew.brinkmann@hotmail.com>
2022-05-18 20:46:50 -0700

    build: adding task 5 and test files

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+'''
+This module is designed to supply 1 funcion for use
+the text_indentation function.
+'''
+
+
+def text_indentation(text):
+    '''
+    function that prints a text with 2 new lines
+    after each of these characters: ., ? and :
+    text must be a string
+    There should be no space at the beginning or at the end of each printed line
+    '''
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    lastCharModified = True
+    for ch in text:
+        if lastCharModified is True:
+            if ch == " ":
+                continue
+            else:
+                lastCharModified = False
+        if ch == "." or ch == ":" or ch == "?":
+            lastCharModified = True
+            print(ch)
+            print()
+        else:
+            print(ch, end="")
