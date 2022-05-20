ee338aa1ece641446da3d1dfae263069f50fa048
Matthew Brinkmann <matthew.brinkmann@hotmail.com>
2022-05-19 21:46:19 -0700

    fix: pycodestyle validaiton error

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -10,7 +10,8 @@ def text_indentation(text):
     function that prints a text with 2 new lines
     after each of these characters: ., ? and :
     text must be a string
-    There should be no space at the beginning or at the end of each printed line
+    There should be no space at the beginning or
+    at the end of each printed line
     '''
     if not isinstance(text, str):
         raise TypeError("text must be a string")
