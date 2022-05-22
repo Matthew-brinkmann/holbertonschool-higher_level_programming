19fe43bc82dea33396f08015e0b6d6f81275db8c
Matthew Brinkmann <matthew.brinkmann@hotmail.com>
2022-05-22 16:17:59 -0700

    fix: Docustring

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -14,23 +14,28 @@ class Rectangle:
 
     @property
     def width(self):
+        """ getter for width attribute"""
         return (self.__width)
 
     @width.setter
     def width(self, value):
+        """ setter for width attribute"""
         self.data_validation(value, "width")
         self.__width = value
 
     @property
     def height(self):
+        """ getter for height attribute"""
         return (self.__height)
 
     @height.setter
     def height(self, value):
+        """ setter for height attribute"""
         self.data_validation(value, "height")
         self.__height = value
 
     def data_validation(self, value, attribute):
+        """ data_validation method for all attributes"""
         if not isinstance(value, int):
             raise TypeError(attribute + " must be an integer")
         if value < 0:
