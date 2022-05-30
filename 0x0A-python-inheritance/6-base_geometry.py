f3a8339bd9b6b92700ee217434f7fc8d0987f3bd
Matthew Brinkmann <matthew.brinkmann@hotmail.com>
2022-05-29 18:38:26 -0700

    build: adding task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+module creates a class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """
+    class for BaseGeometry
+    """
+    def area(self):
+        """
+        causes exception when called
+        """
+        raise Exception("area() is not implemented")
