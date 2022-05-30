7134bc1ca12cc12e2f302147876302935b8e9469
Matthew Brinkmann <matthew.brinkmann@hotmail.com>
2022-05-29 17:58:02 -0700

    fix: pycodestyle error

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -2,6 +2,7 @@
 """this module is to be used for returning
 methonds and attributes of objects"""
 
+
 def lookup(obj):
     """ will returna list of attributes and methods"""
     return (dir(obj))
