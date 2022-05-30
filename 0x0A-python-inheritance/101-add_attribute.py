2988aff10636519e9861e42a43b0230deb40f774
Matthew Brinkmann <matthew.brinkmann@hotmail.com>
2022-05-30 04:00:46 -0700

    build: fix class and module docs

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
-
+"""
+Module to add attribute to compatible classes/ objects
+"""
 
 def add_attribute(obj, name, value):
+    """
+    runs check on data, then if object is compatible,
+    adds attribute to class
+    """
     if hasattr(obj, "__dict__") is False:
         raise TypeError("can't add new attribute")
     setattr(obj, name, value)
