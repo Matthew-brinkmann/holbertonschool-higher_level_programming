121d798f739c76dc6fc9d1b5d5a52fb4a3a191c9
Matthew Brinkmann <matthew.brinkmann@hotmail.com>
2022-05-29 17:53:29 -0700

    build: adding task 0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""this module is to be used for returning
+methonds and attributes of objects"""
+
+def lookup(obj):
+    """ will returna list of attributes and methods"""
+    return (dir(obj))
