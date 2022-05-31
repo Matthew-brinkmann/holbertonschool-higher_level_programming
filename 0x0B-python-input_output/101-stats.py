6449e93e3599d81de8a6afe081e5bed6190ef906
Matthew Brinkmann <matthew.brinkmann@hotmail.com>
2022-05-31 05:25:20 -0700

    fix: removed f strings to test checker response

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -30,12 +30,13 @@ relating to th e
         """
         if self.total_size == 0:
             return ("")
-        retStr = f"File size: {self.total_size}\n"
+        retStr = "File size: " + str(self.total_size) + "\n"
         for code in self.eCodes:
             value = getattr(self, self.code_to_attr(code))
             if value == 0:
                 continue
-            retStr += f"{code}: {getattr(self, self.code_to_attr(code))}\n"
+            retStr += code + ": "
+            retStr += str(getattr(self, self.code_to_attr(code))) + "\n"
         retStr = retStr[:-1]
         return (retStr)
 
