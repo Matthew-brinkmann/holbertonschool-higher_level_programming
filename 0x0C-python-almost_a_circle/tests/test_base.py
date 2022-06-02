551bc2bc24114b1b668f7af3d9d4f4d3ae625f71
Matthew Brinkmann <matthew.brinkmann@hotmail.com>
2022-06-02 03:59:07 -0700

    fix: changed to match cohort gloabl tests

--- 0x0C-python-almost_a_circle/tests/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -4,12 +4,34 @@ Contains tests for Base class
 """
 
 import unittest
+import inspect
+import pycodestyle
 
 from models import base
 Base = base.Base
 
+
 class TestBaseDocs(unittest.TestCase):
     """ Tests for documentation of class"""
+
+    @classmethod
+    def setUpClass(cls):
+        """Set up for the doc tests"""
+        cls.base_funcs = inspect.getmembers(Base, inspect.isfunction)
+
+    def test_conformance_class(self):
+        """Test that we conform to Pycodestyle."""
+        style = pycodestyle.StyleGuide(quiet=True)
+        result = style.check_files(['models/base.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
+
+    def test_conformance_test(self):
+        """Test that we conform to Pycodestyle."""
+        style = pycodestyle.StyleGuide(quiet=True)
+        result = style.check_files(['tests/test_base.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
 
     def test_module_docstr(self):
         """ Tests for docstring"""
@@ -18,6 +40,12 @@ class TestBaseDocs(unittest.TestCase):
     def test_class_docstr(self):
         """ Tests for docstring"""
         self.assertTrue(len(Base.__doc__) >= 1)
+
+    def test_func_docstr(self):
+        """Tests for docstrings in all functions"""
+        for func in self.base_funcs:
+            self.assertTrue(len(func[1].__doc__) >= 1)
+
 
 class TestBase(unittest.TestCase):
     """ Tests functionality of class"""
--- 0x0C-python-almost_a_circle/tests/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_rectangle.py
@@ -1,51 +1,96 @@
 #!/usr/bin/python3
 """
-Contains tests for Base class
+Contains tests for Rectangle class - Task 2
+Focus is on:
+- setting private instance attributes in the constructor using
+  getters and setters
+- calling super()__init__() to set public instance id attribute
 """
 
 import unittest
+import inspect
+import pycodestyle
 
-from models import rectangle
 from models.base import Base
+from models import rectangle
 Rectangle = rectangle.Rectangle
 
-class TestRectangeDocs(unittest.TestCase):
-    """ Tests for documentation of class"""
+
+class TestRectangleDocs(unittest.TestCase):
+    """Tests for documentation of class"""
+
+    @classmethod
+    def setUpClass(cls):
+        """Set up for the doc tests"""
+        cls.rect_funcs = inspect.getmembers(Rectangle, inspect.isfunction)
+
+    def test_conformance_class(self):
+        """Test that we conform to Pycodestyle."""
+        style = pycodestyle.StyleGuide(quiet=True)
+        result = style.check_files(['models/rectangle.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
+
+    def test_conformance_test(self):
+        """Test that we conform to Pycodestyle."""
+        style = pycodestyle.StyleGuide(quiet=True)
+        result = style.check_files(['tests/test_rectangle.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
 
     def test_module_docstr(self):
-        """ Tests for docstring"""
+        """Tests for docstring"""
         self.assertTrue(len(rectangle.__doc__) >= 1)
 
     def test_class_docstr(self):
-        """ Tests for docstring"""
+        """Tests for docstring"""
         self.assertTrue(len(Rectangle.__doc__) >= 1)
+
+    def test_func_docstr(self):
+        """Tests for docstrings in all functions"""
+        for func in self.rect_funcs:
+            self.assertTrue(len(func[1].__doc__) >= 1)
+
 
 class TestRectangle(unittest.TestCase):
     """ Tests functionality of class"""
-    def test_id_none(self):
-        """ Tests id as none and heigh and width set"""
-        Base._Base__nb_objects = 0
-        r1 = Rectangle(10, 2)
-        self.assertEqual(r1.id, 1)
-        self.assertEqual(r1.width, 10)
-        self.assertEqual(r1.height, 2)
-        self.assertEqual(r1.x, 0)
-        self.assertEqual(r1.y, 0)
 
-        r1.x = 10
-        r1.y = 10
-        self.assertEqual(r1.x, 10)
-        self.assertEqual(r1.y, 10)
+    def test_properties(self):
+        """Tests all setters and getters"""
+        Base._Base__nb_objects = 0
+        r_1 = Rectangle(1, 1)
+        self.assertEqual(r_1.width, 1)
+        self.assertEqual(r_1.height, 1)
+        self.assertEqual(r_1.x, 0)
+        self.assertEqual(r_1.y, 0)
+
+        r_1.x = 10
+        r_1.y = 10
+        self.assertEqual(r_1.x, 10)
+        self.assertEqual(r_1.y, 10)
+
+    """ Tests functionality of super()__init__() call"""
+
+    def test_id_none(self):
+        """Tests id not passed in"""
+        r_2 = Rectangle(1, 1)
+        self.assertEqual(r_2.id, 2)
 
     def test_id_assigned(self):
-        """ Tests id is give"""
-        r2 = Rectangle(10, 2, 0, 0, 12)
-        self.assertEqual(r2.id, 12)
+        """Tests id passed in"""
+        r_3 = Rectangle(1, 1, 1, 1, 88)
+        self.assertEqual(r_3.id, 88)
 
+    def test_nb_object_increment(self):
+        """Tests private class attribute incrementation"""
+        r_4 = Rectangle(1, 1)
+        self.assertEqual(r_4.id, 3)
+
+    """Tests number of arguments passed in"""
     def test_too_many_args(self):
-        """ Tests entering too many args to instantiate class"""
+        """Tests entering too many args to instantiate object"""
         with self.assertRaises(TypeError):
-            r3 = Rectangle(10, 2, 0, 0, 12, 12)
+            r = Rectangle(1, 2, 3, 4, 5, 6)
 
     def test_too_few_args(self):
         """Tests entering too few args to instantiate object"""
