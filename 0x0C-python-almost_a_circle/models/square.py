9007d0856745d8e3bc7d28a0ac8e44c2152eea16
Matthew Brinkmann <matthew.brinkmann@hotmail.com>
2022-06-02 16:25:57 -0700

    build: adding size attribute and using width, height setters

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -12,9 +12,21 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """ init for square"""
         super().__init__(size, size, x, y, id)
+        self.size = size
 
     def __str__(self):
         """returns informal representation of string"""
         retStr = f"[{self.__class__.__name__}] ({self.id}) "
         retStr += f"{self.x}/{self.y} - {self.width}"
         return (retStr)
+
+    @property
+    def size(self):
+        """getter for size"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """setter for size"""
+        self.width = value
+        self.height = value
