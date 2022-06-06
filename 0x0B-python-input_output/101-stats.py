084793cc3f58332c01a298281e8c79a0efa63895
Matthew Brinkmann <matthew.brinkmann@hotmail.com>
2022-06-07 07:51:51 +1000

    fix: adding feedback from code review

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -12,6 +12,7 @@ class Metrics:
     metrics class to handle holding and display of information
 relating to th e
     """
+    #change variable names to be more readable
     eCodes = ['200', '301', '400', '401', '403', '404', '405', '500']
 
     def __init__(self):
@@ -22,31 +23,38 @@ relating to th e
         self.total_size = 0
         self.lineCount = 0
         for code in self.eCodes:
+            #store values in a dictionary
             setattr(self, self.code_to_attr(code), 0)
 
     def __str__(self):
         """
         informal string for printing data
         """
+        #ensure variable naming conventions match
         retStr = f"File size: {self.total_size}\n"
         for code in self.eCodes:
             value = getattr(self, self.code_to_attr(code))
             if value == 0:
                 continue
+            #change add newline to here
             retStr += f"{code}: {getattr(self, self.code_to_attr(code))}\n"
+        #remove this line to avoid "segmentation faults"
         retStr = retStr[:-1]
         return (retStr)
 
     def code_check(self, Code, size):
         """
         checks if attribute exists and updates fields if required
+        Name to be more specific with naming, Method is incrementing not checking.
         """
+        #varaible names need to state purpose
         if Code in self.eCodes:
             try:
                 self.total_size += int(size)
                 value = getattr(self, self.code_to_attr(Code)) + 1
                 setattr(self, self.code_to_attr(Code), value)
             except Exception:
+                #need more specification.
                 return
         else:
             return
