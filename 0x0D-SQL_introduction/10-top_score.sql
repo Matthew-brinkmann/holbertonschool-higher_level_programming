33b527cd3eb1243b4858fd335bb4c6b9c9efca65
Matthew Brinkmann <matthew.brinkmann@hotmail.com>
2022-07-04 20:04:09 -0700

    build: adding task 10

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,5 @@
+-- lists all records of the table second_table
+-- display both the score and the name (in this order)
+-- Records should be ordered by score (top first)
+-- DB will be passed as an argument
+SELECT score, name FROM second_table ORDER BY score DESC;
