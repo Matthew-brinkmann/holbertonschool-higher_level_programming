643bb880f81ca7cfc238f3029d55df4b4f91a3ef
Matthew Brinkmann <matthew.brinkmann@hotmail.com>
2022-07-04 19:30:56 -0700

    build: adding task 5

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,3 @@
+-- prints the full desc. of the table first_table from the database hbtn_0c_0
+-- Create Table contains string of data that is used to create the table
+SHOW CREATE TABLE first_table;
