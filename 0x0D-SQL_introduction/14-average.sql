2b5b5b363465d04c832fa54e92665497dc8ddbef
Matthew Brinkmann <matthew.brinkmann@hotmail.com>
2022-07-05 03:59:49 -0700

    build; adding task 14

---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,4 @@
+-- computes the score average of all records in the table second_table
+-- The result column name should be average
+-- DB will be passed as an argument
+SELECT AVG(score) AS average FROM second_table;
