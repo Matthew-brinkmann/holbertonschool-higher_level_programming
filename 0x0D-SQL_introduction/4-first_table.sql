9ac11eb18fc20a648793db75581912088c606dff
Matthew Brinkmann <matthew.brinkmann@hotmail.com>
2022-07-04 19:24:28 -0700

    build: adding task 4

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,3 @@
+-- creates a table called first_table in the current database in MySQL server.
+-- contains id (INT) and name (VARCHAR256)
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
