3a10d1d6b0c47cf2e8af4ca8513f41692c8cf71b
Matthew Brinkmann <matthew.brinkmann@hotmail.com>
2022-07-04 20:01:46 -0700

    fix: change the format of the answer

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -4,11 +4,13 @@
 -- score INT
 -- and insert Users
 -- DB will be passed as na argument
-CREATE TABLE IF NOT EXISTS second_table (id INT,
+CREATE TABLE IF NOT EXISTS second_table (
+id INT,
 name VARCHAR(256),
 score INT);
 INSERT INTO second_table (id, name, score)
-VALUES (1, "John", 10),
+VALUES
+(1, "John", 10),
 (2, "Alex", 3),
 (3, "Bob", 14),
 (4, "George", 8);
