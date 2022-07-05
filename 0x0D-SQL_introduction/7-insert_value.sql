f40e91847f515ee82da20d739f03b86943ad2893
Matthew Brinkmann <matthew.brinkmann@hotmail.com>
2022-07-04 19:49:52 -0700

    fix: ' change to double quotes

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,3 @@
  -- inserts a new row in the table first_table
  -- DB will be passed as argument
- INSERT INTO first_table (id, name) VALUES (89, 'Best School');
+ INSERT INTO first_table (id, name) VALUES (89, "Best School");
