38a87f29565679ea41dbeb51e1e348700b890d92
Matthew Brinkmann <matthew.brinkmann@hotmail.com>
2022-08-01 12:37:39 +1000

    fix: typo

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const stringsToPrint = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const stringsToPrint = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 let arrayIdx = 0;
 while (arrayIdx < stringsToPrint.length) {
   console.log(stringsToPrint[arrayIdx]);
