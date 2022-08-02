65582e59bad117a1b1780126c49b6166d6c071c0
Matthew Brinkmann <matthew.brinkmann@hotmail.com>
2022-08-03 09:54:17 +1000

    build: add task 3

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+'''
+This module is a basic interaction with
+a MYSQL DB
+'''
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    SQLdbConnection = MySQLdb.connect(host="localhost",
+                                      port=3306,
+                                      user=sys.argv[1],
+                                      passwd=sys.argv[2],
+                                      db=sys.argv[3])
+    dbCurser = SQLdbConnection.cursor()
+    dbCurser.execute(f'''
+                      SELECT * FROM states
+                      ORDER BY id ASC
+                      ''')
+    query_rows = dbCurser.fetchall()
+    for row in query_rows:
+        if (sys.argv[4] == row[1]):
+            print(row)
+    dbCurser.close()
+    SQLdbConnection.close()
