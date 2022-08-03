720960ea18fc636148ec6c4ea5c203fe67a02ecb
Matthew Brinkmann <matthew.brinkmann@hotmail.com>
2022-08-03 11:22:33 +1000

    Fix: change to f-string

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -15,5 +15,5 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
     for state in session.query(State).order_by(State.id):
-        print('{}: {}'.format(state.id, state.name))
+        print(f'{state.id}: {state.name}')
     session.close()
