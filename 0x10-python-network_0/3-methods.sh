1b7accfe526b200ed8f09038ba7553ba4aa2b6b0
Matthew Brinkmann <matthew.brinkmann@hotmail.com>
2022-08-16 20:25:37 -0700

    fix: remove test input

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes in a URL and displays all HTTP methods the server will accept.
-curl -sI 0.0.0.0:5000/route_4 | grep -w "Allow:" | cut -d' ' -f2-
+curl -sI "$1" | grep -w "Allow:" | cut -d' ' -f2-
