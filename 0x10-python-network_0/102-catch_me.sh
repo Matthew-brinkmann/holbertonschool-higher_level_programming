eae545f1ef91528d132151ddf89b1adbec460e11
Matthew Brinkmann <matthew.brinkmann@hotmail.com>
2022-08-16 20:57:06 -0700

    fix: comment typo"

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Full story of how i achieved this is in advanced_task_2.txt
+# Full story of how i achieved this is in advanced_task_2.md
 curl -sLX PUT -d "user_id=98" -H "Origin:HolbertonSchool" 0.0.0.0:5000/catch_me
