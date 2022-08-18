f344eef7bdcd8323efd95058e9479bbaea5d1273
Matthew Brinkmann <matthew.brinkmann@hotmail.com>
2022-08-19 09:47:10 +1000

    build: add task 7

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+takes in a URL, sends a request to the URL and displays
+the body of the response.
+If the HTTP status code is greater than or equal to 400,
+print: Error code: followed by the value of the HTTP status code
+"""
+from requests import get
+
+
+if __name__ == "__main__":
+    urlToQuery = 'https://intranet.hbtn.io/status'
+    responseContent = get(urlToQuery)
+    if responseContent.ok:
+        print(responseContent.text)
+    else:
+        print('Error code: {}'.format(responseContent.status_code))
