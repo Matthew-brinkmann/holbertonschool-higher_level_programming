0c15f0b7a93ec89d1e2144e2b4e4045f30f78c9f
Matthew Brinkmann <matthew.brinkmann@hotmail.com>
2022-08-19 09:35:23 +1000

    build: add task 5

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+script that fetches https://intranet.hbtn.io/status
+"""
+from requests import get
+import sys
+
+
+if __name__ == "__main__":
+    urlToQuery = sys.argv[1]
+    responseContent = get(urlToQuery)
+    print(responseContent.headers['X-Request-Id'])
