74adc2e7827ad3e6928ca8131ad993542e452f0e
Matthew Brinkmann <matthew.brinkmann@hotmail.com>
2022-08-19 08:12:51 +1000

    Build: add task 0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''
+script that fetches https://intranet.hbtn.io/status
+'''
+if __name__ == "__main__":
+    import urllib.request
+    url = "https://intranet.hbtn.io/status"
+    with urllib.request.urlopen(url) as response:
+        content = response.read()
+        print('Body response:')
+        print(f"\t- type: {type(content)}")
+        print(f"\t- content: {content}")
+        print(f"\t- utf8 content: {content.decode('utf-8')}")
