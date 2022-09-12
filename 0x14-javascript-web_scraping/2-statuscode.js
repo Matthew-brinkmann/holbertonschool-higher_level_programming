b56007137caf3d4d3c8ee3c503f71069ef9a60fd
Matthew Brinkmann <matthew.brinkmann@hotmail.com>
2022-09-13 09:28:46 +1000

    fix

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,10 +2,8 @@
 const url = process.argv[2];
 const axios = require('axios');
 axios.get(url)
-  .then(function (response, error) {
-    if (response) {
-      console.log('code: ' + response.status);
-    } else {
-      console.log('code: ' + error.response.status);
-    }
+  .then(function (response) {
+    console.log('code: ' + response.status);
+  }).catch(function (error) {
+    console.log('code: ' + error.response.status);
   });
